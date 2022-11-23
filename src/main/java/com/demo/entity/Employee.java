d11e4d1a3bde488d2969bff375ecc014d16645d4
pawankumargithub <pkgupta789428@gmail.com>
2022-11-23 11:42:02 +0530

    2nd commit

--- src/main/java/com/demo/entity/Employee.java
+++ src/main/java/com/demo/entity/Employee.java
@@ -1,14 +1,23 @@
 package com.demo.entity;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Employee {
 
 	private int id;
 	private String name;
+	public int getId() {
+		return id;
+	}
+	public Employee(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
